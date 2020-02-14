0d11f7b48bd0715d57efdb07300a910014584af5
Luckaa <lucasilva137@gmail.com>
2020-02-14 18:28:03 -0400

    Request para os usuarios
    request dos usuarios sendo exibidos no console

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <div class="card card-body">
         <h1>Pesquisar repositorios</h1>
         <p class="lead">digite o nome do repositorio que você deseja</p>
-        <input class="form-control" id="search" type="text" required>
+        <input @keyup="getUser" class="form-control" id="search" type="text" required>
       </div>
     </div>
   </div>
@@ -13,22 +13,33 @@
 
 <script>
 import Navbar from'./components/Navbar.vue';
+import axios from "axios";
 
 export default {
   name: 'App',
   data(){
     return{
       github:{
-        url:'',
-        client_id:'',
-        client_secret:'',
+        url:'https://api.github.com/users',
+        client_id:'40a57736385abed0fc27',
+        client_secret:'7d5ef09a9c9c8b5b22d1d17c5b4afcc5f38224d1',
         count: 7,
         sort:'created: asc'
-      }
+      },
+      user:[],
+      repos:[]
     }
   },
   components:{
     Navbar
+  },
+  methods:{
+    getUser(e){
+      const user = e.target.value;
+      const {url,client_id,client_secret,count,sort} = this.github
+      axios.get(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`) 
+      .then(({data})=>this.user = console.log(data))
+    }
   }
 }
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-dark bg-success mb-3">
         <div class="container">
-            <a href="#" class="navbar-brand">Pesquisa GitHub</a>
+            <a href="#" class="navbar-brand">Pesquisar por:</a>
         </div>
     </nav>   
 </template>
