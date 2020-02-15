dca7ee4093300d273d4537a3d6646202f5a16317
Luckaa <lucasilva137@gmail.com>
2020-02-15 17:15:02 -0400

    total adicionado
    total adicionado para dizer a linguagem escolhida e o total de repositorios

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,57 @@
 <template>
   <div id="app">
         <div>
-      <h5>Pesquisar por  <a @click="determinarUser" id="usua" href="#">Usuarios</a> ou 
-    <a @click="determinarTec" id="tec" href="#">Tecnologias</a></h5>
+      <h5>Pesquisar por  <a @click="determinarUser"  href="#">Usuarios</a> ou 
+    <a @click="determinarTec"  href="#">Tecnologias</a></h5>
   
 
-    <h5>Lista de  <a href="#">Favoritos</a></h5>
+    <h5>Lista de  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Favoritos</a></h5>
   
      
     </div>
 
-    <div class="container">
+    <div class="row mt-3">
 
 
         <div v-if="visibilidadeUser !== false" class="card card-body">
           <h1>Usuarios</h1>
-          <p class="lead">digite o nome do Usuario que você deseja pesquisar</p>
+          <p class="lead">digite o nome do usuario que você deseja pesquisar</p>
           <input @keyup="getUser" class="form-control" id="search" type="text" required>
         </div>
+
+          <div v-if="visibilidadeTec !== false" class="card card-body">
+          <h1>Tecnologias</h1>
+          <p class="lead">digite o nome da tecnologia que você deseja pesquisar</p>
+          <input @keyup="getTec" class="form-control" id="searc" type="text" required>
+          </div>
 
   <div  v-if="user.length !== 0" class="row mt-3"> 
     <div class="col-md-4">
       <profile :user="user"/>
     </div>
 
+    <div class="col-md-8">
+      <Repo v-for="repo in repos" :key="repo" :repo="repo"/>
+    </div>
+    </div>
+    <div  v-if="tec.length !== 0" class="row mt-3"> 
+    <div class="col-md-4">
+      <Total :tec="tec"/>
+    </div>
 
     <div class="col-md-8">
       <Repo v-for="repo in repos" :key="repo" :repo="repo"/>
     </div>
     </div>
+
+
     </div>
   </div>
 </template>
 <script>
 import Profile from'./components/Profile.vue';
 import Repo from'./components/Repo.vue';
+import Total from'./components/Total.vue';
 
 
 import axios from "axios";
@@ -53,33 +70,44 @@ export default {
       
 
       github:{
-        url:'https://api.github.com/users',
+        urlTec:"https://api.github.com" ,
+        urlUser:'https://api.github.com/users',
         client_id:'40a57736385abed0fc27',
         client_secret:'7d5ef09a9c9c8b5b22d1d17c5b4afcc5f38224d1',
         count: 1000,
         sort:'created: asc'
       },
       user:[],
+      tec:[],
       repos:[],
       visibilidadeUser:false,
-      visibilidadeTec:false
+      visibilidadeTec:false,
     }
   },
   components:{
     Profile,
-    Repo
+    Repo,
+    Total
     
   },
   methods:{
     getUser(e){
       const user = e.target.value;
-      const {url,client_id,client_secret,count,sort} = this.github
+      const {urlUser,client_id,client_secret,count,sort} = this.github
 
-      axios.get(`${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`) 
+      axios.get(`${urlUser}/${user}?client_id=${client_id}&client_secret=${client_secret}`) 
       .then(({data})=>this.user = (this.user = data))
 
-      axios.get(`${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`)
+      axios.get(`${urlUser}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`)
       .then(({data})=> this.repos = data)
+    },
+    getTec(e){
+      const tec = e.target.value;
+      const {urlTec,client_id,client_secret} = this.github
+
+      axios.get(`${urlTec}/search/repositories?q=language:${tec}&sort=stars&page=1&client_id=${client_id}&client_secret=${client_secret}`) 
+      .then(({data})=>this.tec = (this.tec = data))
+
     },
     determinarNada(){
       console.log(visibilidadeTec,visibilidadeUser)
---
+++ src/components/Total.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card" style="witdth: 18rem;">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Linguagem Selecionada: <span class="badge-success">{{tec.items[0].language}}</span></li>
+                        <li class="list-group-item">Repositórios: <span class="badge-primary">{{tec.total_count}}</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        props:["tec"]
+        }
+</script>
